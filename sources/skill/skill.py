685a9ded4762f8e5f6aa0d0c812c72e200a46e31
pbillerot <philippe.billerot@gmail.com>
2019-09-13 15:31:46 +0200

    ça sera tout pour aujourd'hui

--- sources/skill/skill.py
+++ sources/skill/skill.py
@@ -39,11 +39,11 @@ logger.setLevel(logging.INFO)
 
 
 def get_slot_id(slot):
-  print(f"get_slot_id:{slot}")
+  # print(f"get_slot_id:{slot}")
   try:
     if slot.resolutions is not None:
       status = slot.resolutions.resolutions_per_authority[0].status.code
-      print(f"for {slot.name} status={status}")
+      # print(f"for {slot.name} status={status}")
       if status == StatusCode.ER_SUCCESS_MATCH:
         id = slot.resolutions.resolutions_per_authority[0].values[0].value.id
         return id
